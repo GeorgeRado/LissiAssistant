4114ac0ce22cb72bf8ebab5cf431c5918f5571d4
Geroge Rado <g.rado@rado-pm.com>
2025-01-30 03:02:25 +0100

    Clean commit

---
+++ LissiAssistant/LissiAssistantTests/LissiAssistantTests.swift
@@ -0,0 +1,16 @@
+//
+//  LissiAssistantTests.swift
+//  LissiAssistantTests
+//
+//  Created by George Rado on 30.01.25.
+//
+
+import Testing
+
+struct LissiAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
